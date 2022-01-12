bc3b25e7d2473143def0443abb2a84897ecb0d27
Burak <burak.yildiz0@outlook.com>
2022-01-12 17:20:49 +0300

    Add user repository

---
+++ src/main/java/com/burakyildiz/springboothomework4/repository/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.burakyildiz.springboothomework4.repository;
+
+import com.burakyildiz.springboothomework4.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Long>
+{
+    Optional<User> findByUsername(String username);
+}
